3dac6217b841be7a34f41001a8778560c806e461
BenRango <srangoelie@gmail.com>
2026-01-26 02:33:54 +0000

    chore: updated baseApi

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use('/moneytransfer/api/v1', routes ); 
+app.use('/api/v1', routes ); 
 app.use('', (req, res) => {
     console.log(`${req.method} ${req.url}`);
     res.status(404).send('Not Found');
